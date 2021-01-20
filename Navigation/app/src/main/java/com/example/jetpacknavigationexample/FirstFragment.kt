14bc31e214dab8c87c051e7391da16059f1fa28e
kaaneneskpc <kaaneneskpc1@gmail.com>
2021-01-20 19:21:12 +0300

    KotlinJetpack

---
+++ Navigation/app/src/main/java/com/example/jetpacknavigationexample/FirstFragment.kt
@@ -0,0 +1,39 @@
+package com.example.jetpacknavigationexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        first_fragment_button.setOnClickListener {
+            val action =  FirstFragmentDirections.actionFirstFragmentToSecondFragment(40)
+            Navigation.findNavController(it).navigate(action)
+        }
+
+    }
+
+
+
+}
---
+++ Navigation/app/src/main/java/com/example/jetpacknavigationexample/SecondFragment.kt
@@ -0,0 +1,42 @@
+package com.example.jetpacknavigationexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.*
+
+
+class SecondFragment : Fragment() {
+
+    private var myAge = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            myAge = SecondFragmentArgs.fromBundle(it).age
+            second_Fragment_TextView.setText(myAge.toString())
+        }
+
+        second_fragment_button.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
